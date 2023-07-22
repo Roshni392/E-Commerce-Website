4a5003cc644e22b7c72e04ad0ce1b5abae26ad1a
Roshni392 <140273426+Roshni392@users.noreply.github.com>
2023-07-23 00:02:52 +0530

    Add files via upload

---
+++ project/js/script.js
@@ -0,0 +1,115 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+    //cart
+// Define an array to store cart items
+let cartItems = [];
+
+// Define a function to add an item to the cart
+function addToCart(item) {
+  // Check if the item is already in the cart
+  let existingItem = cartItems.find(cartItem => cartItem.id === item.id);
+
+  if (existingItem) {
+    // If the item already exists in the cart, increase its quantity
+    existingItem.quantity += 1;
+  } else {
+    // If the item does not exist in the cart, add it with a quantity of 1
+    cartItems.push({...item, quantity: 1});
+  }
+
+  // Update the UI to reflect the cart changes
+  updateCartUI();
+}
+
+// Define a function to update the UI with cart information
+function updateCartUI() {
+  // Get the cart count element from the DOM
+  let cartCount = document.getElementById('cart-count');
+
+  // Update the cart count with the number of items in the cart
+  cartCount.innerHTML = cartItems.length;
+}
+
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
